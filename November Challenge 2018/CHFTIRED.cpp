76d46da2b50b7ff283bae54f096fce3b38504351
prashantbarthwal10 <prashantbarthwal@outlook.com>
2018-11-07 14:12:58 +0530

    #1 commit

---
+++ November Challenge 2018/CHFTIRED.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        long long int size;
+        long long int a,b;
+        cin>>size;
+        while(size--)
+        {
+                cin>>a>>b;
+                if(a!=b)
+                        cout<<"YES"<<endl;
+                else
+                        cout<<"NO"<<endl;
+
+        }
+
+
+
+        return 0;
+}
